e8731ab0e71c838fbd1f582e3bfaa15abd6b1b2a
Shannon-Bouma <126546740+Shannon-Bouma@users.noreply.github.com>
2024-09-21 13:36:29 +1000

    Added base and default voice model strategies

--- project/src/tts/default_strategy.py
+++ project/src/tts/default_strategy.py
@@ -2,6 +2,7 @@ import time
 import numpy as np
 
 from src.tts.tts_strategy import TTSStrategy
+from src.tts.BaseVoiceModelStrategy import BaseVoiceModelStrategy
 
 class DefaultStrategy(TTSStrategy):
     """
@@ -9,17 +10,18 @@ class DefaultStrategy(TTSStrategy):
     This allows for a default voice to be used, no voice clone, etc.
 
     methods:
-        generate_speech(chunk: str):
-            Generate speech from a chunk of text. Can be overridden by subclasses 
+        generate_speech(chunk: str, voice: BaseVoiceModelStrategy):
+            Generate speech from a chunk of text and voice . Can be overridden by subclasses 
             for more specific implementations, such as generating male or female voices.
     """
     
-    def generate_speech(self, chunk: str):
+    def generate_speech(self, chunk: str, voice: BaseVoiceModelStrategy):
         """
         Concrete method for generating default speech for the given text chunk.
 
         param: chunk (str): A string representing the segment of text to be converted 
                             into speech.
+        param: voice (BaseVoiceModelStrategy): The voice model to be used for speech generation.
 
         return: np.ndarray: ********[FILL IN HERE ONCE DONE]********
         """
--- project/src/tts/tts_strategy.py
+++ project/src/tts/tts_strategy.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 
+from src.tts.BaseVoiceModelStrategy import BaseVoiceModelStrategy
+
 class TTSStrategy(ABC):
     """
     Abstract Base Class for defining a Text-To-Speech (TTS) strategy. Utilises Python built-in
@@ -8,16 +10,17 @@ class TTSStrategy(ABC):
     param: ABC (Abstract Base Class): Python built-in abstract class base declaration.
     
     methods:
-        generate_audio(chunk: str):
-            Abstract method. Takes a chunck of text (string) as input. 
+        generate_speech(chunk: str, voice: BaseVoiceModelStrategy):
+            Abstract method. Takes a chunck of text (string) and voice model as input. 
             Output is returned in the desired format.
     """
     @abstractmethod
-    def generate_speech(self, chunk: str):
+    def generate_speech(self, chunk: str, voice: BaseVoiceModelStrategy):
         """
         Generate speech from the given text chunk.
 
         param: chunk (str): chunk of text to be converted to speech.
+        param: voice (BaseVoiceType): voice model to use to generate speech.
         
         return: None: Return type depends on concrete method.
         """
