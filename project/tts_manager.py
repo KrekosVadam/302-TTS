15d85b501e6e6861e820d6ca0daade4f66c3f34c
Gurleen Kaur <gurleenka2001@gmail.com>
2024-09-28 16:06:58 +0800

    Update tts_manager.py

--- project/tts_manager.py
+++ project/tts_manager.py
@@ -2,10 +2,10 @@
 import numpy as np
 import simpleaudio as sa
 
-from tts.tts_strategy import TTSStrategy
-from tts.male_default_strategy import MaleDefaultStrategy
-from tts.female_default_strategy import FemaleDefaultStrategy
-from tts.custom_strategy import CustomStrategy
+from .tts.tts_strategy import TTSStrategy
+from .tts.male_default_strategy import MaleDefaultStrategy
+from .tts.female_default_strategy import FemaleDefaultStrategy
+from .tts.custom_strategy import CustomStrategy
 
 class TTSManager:
     """
