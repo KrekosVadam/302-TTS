1bf6d6406ed8e32e0c1aa0f1b9578cfe4c0c19ef
kauriwave <gurleenka2001@gmail.com>
2024-10-10 17:11:30 +0800

    Deleted src/stream directory and its contents

--- project/src/stream/multi_thread_strategy.py
+++
@@ -1,6 +0,0 @@
-import re
-from src.stream.strategies.stream_strategies import StreamStrategy
-
-class MultiThreadStream(StreamStrategy):
-    def stream(self, text: str, max_words: int):
-        pass
