9bc3fc030983bdb59aa25113e57497ee58e59c9e
Gurleen Kaur <gurleenka2001@gmail.com>
2024-10-11 10:51:19 +0800

    Update main.py

--- project/main.py
+++ project/main.py
@@ -1,6 +1,5 @@
-from project.tts_manager import TTSManager
-
-import onnx
+import time
+from project.tts_manager import TTSManager 
 
 def main():
     """Main function to select strategy and process text-to-speech."""
@@ -13,13 +12,25 @@ def main():
     voice_type = 'male'
 
     # Hardcode text as a list
-    #samples = ['Hey, I just ','got off work','and thought','I would give','you a call.']
     samples = ['Hey, I just got off work and thought I would give you a call.']
 
     # Process each text sample
     for sample in samples:
         print(f"Processing sample: {sample}")
+        
+        # Start time measurement
+        start_time = time.time()
+        
+        # Call the process method
         tts_manager.process(sample, voice_type)
+        
+        # End time measurement
+        end_time = time.time()
+        
+        # Calculate inference time
+        inference_time = end_time - start_time
+        
+        print(f"Inference time for sample: {inference_time:.4f} seconds")
 
 # Ensure the script runs when executed directly
 if __name__ == "__main__":
